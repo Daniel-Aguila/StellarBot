b6c8accb617ee2da633b49d39d73842912210adb
Daniel Aguila <47072469+Daniel-Aguila@users.noreply.github.com>
2022-09-02 06:49:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    elif message.content.startswith('!hello'):
         await message.channel.send('Hello ' + message.author.name + '!')
 
-    if message.content.startswith('!cult_hc'):
+    elif message.content.startswith('!cult_hc'):
         reply = await message.channel.send('@here Cult Headcount, hosted by ' + message.author.name + '!')
         emojis = ['✅','❌']
         for emoji in emojis:
             await reply.add_reaction(emoji)
 
-    if message.content.startswith('!o3_hc'):
+    elif message.content.startswith('!o3_hc'):
         reply = await message.channel.send('@here O3 Headcount, hosted by ' + message.author.name + '!')
         emojis = ['✅','❌']
         for emoji in emojis:
             await reply.add_reaction(emoji)
 
-    if message.content.startswith('!shatters_hc'):
+    elif message.content.startswith('!shatters_hc'):
         reply = await message.channel.send('@here Shatters Headcount, hosted by ' + message.author.name + '!')
         emojis = ['✅','❌']
         for emoji in emojis:
